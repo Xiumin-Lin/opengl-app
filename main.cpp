ba8519438e69d6cb460faa6e6e627e800b0e0fe5
Lin <60708580+Xiumin-Lin@users.noreply.github.com>
2024-06-19 15:52:58 +0200

    feat: base position and color

--- main.cpp
+++ main.cpp
@@ -5,45 +5,47 @@
 #include <vector>
 #include <cmath>
 
-#include "./common/GLShader.h"
-
+#include "common/GLShader.h"
 const int WIDTH = 800;
 const int HEIGHT = 600;
-
-struct vec2 { float x, y; };
-struct vec3 { float x, y, z; };
-struct vec4 { float x, y, z, w; };
-
-struct Point
+struct vec2
 {
-    vec2 position;
+    float x, y;
+};
+struct vec3
+{
+    float x, y, z;
+};
+struct vec4
+{
+    float x, y, z, w;
 };
 
-struct Color
+struct Vertex
 {
-    vec3 rgb;
+    vec2 position;
+    vec3 color;
 };
 
 struct Application
 {
-    GLShader g_BasicShader;
-    bool Initialize()
-    {
-        g_BasicShader.LoadVertexShader("./shaders/basic.vs");
-        g_BasicShader.LoadFragmentShader("./shaders/basic.fs");
-        g_BasicShader.Create();
+    GLShader m_basicProgram;
 
-    // cette fonction est spécifique à Windows et permet d’activer (1) ou non (0)
-    // la synchronization vertical. Elle necessite l’include wglew.h
+    void Initialize()
+    {
+        m_basicProgram.LoadVertexShader("./shaders/basic.vs.glsl");
+        m_basicProgram.LoadFragmentShader("./shaders/basic.fs.glsl");
+        m_basicProgram.Create();
+        // cette fonction est spécifique à Windows et permet d’activer (1) ou non (0)
+        // la synchronization vertical. Elle necessite l’include wglew.h
 #ifdef WIN32
         wglSwapIntervalEXT(1);
 #endif
-        return true;
     }
 
     void Terminate()
     {
-        g_BasicShader.Destroy();
+        m_basicProgram.Destroy();
     }
 
     void Render()
@@ -56,41 +58,53 @@ struct Application
         glClear(GL_COLOR_BUFFER_BIT);
 
         // etape c. on specifie le shader program a utiliser
-        auto basicProgram = g_BasicShader.GetProgram();
-        glUseProgram(basicProgram);
+        uint32_t program = m_basicProgram.GetProgram();
+        glUseProgram(program);
 
         // etape d. définition d’une géométrie
         // premier parametre = 0, correspond ici au canal/emplacement du premier attribut
         // glEnableVertexAttribArray() indique que les donnees sont generiques (proviennent
         // d’un tableau) et non pas communes a tous les sommets
-        const Point triangle[] = {
-            {{-0.5f, -0.5f}},
-            {{0.5f, -0.5f}},
-            {{0.0f, 0.5f}}
-        };
+        const std::vector<Vertex> triangles =
+            {
+                {{0.0f, 0.5f}, {1.0f, 0.0f, 0.0f}},   // sommet 0 et couleur rouge
+                {{-0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}}, // sommet 1 et couleur verte
+                {{0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}}   // sommet 2 et couleur bleue
+            };
 
-        int loc_position = glGetAttribLocation(basicProgram, "a_Position");
-        glEnableVertexAttribArray(loc_position);
-        // glVertexAttribPointer(index, taille, type, normalisation, écart, adresse)
-        glVertexAttribPointer(loc_position, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, triangle);
+        // {[x, y], [r, g, b]}
+        const int POSITION = glGetAttribLocation(program, "a_Position");
+        const int COLOR = glGetAttribLocation(program, "a_Color");
+        if (POSITION < 0 || COLOR < 0)
+        {
+            std::cout << "erreur de programme";
+            return;
+        }
 
-        // Exercice A.1 - Colorier le triangle
-        const Color color[] = {
-            {{1.f, 1.f, 0.f}},
-            {{1.f, 0.f, 1.f}},
-            {{0.f, 1.f, 1.f}}
-        };
+        glEnableVertexAttribArray(POSITION);
+        // &triangles[0].position est un pointeur vers le premier élément de la première position dans le tableau triangles.
+        // ensuite chaque sommet est composé de 2 GL_FLOAT et ne sont pas normalisées (GL_FALSE)
+        // et que le stride (l’espacement entre les attributs successifs) est de sizeof(Vertex) (taille d'un sommet)
+        glVertexAttribPointer(POSITION, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex) /*stride*/, &triangles[0].position);
 
-        int loc_color = glGetAttribLocation(basicProgram, "a_Color");
-        glEnableVertexAttribArray(loc_color);
-        glVertexAttribPointer(loc_color, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, color);
-
+        glEnableVertexAttribArray(COLOR);
+        glVertexAttribPointer(COLOR, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex) /*stride*/, &triangles[0].color);
 
         // etape e. paramétrer le rendu
+        int timeLocation = glGetUniformLocation(program, "u_Time");
+        double time = glfwGetTime();
+        glUniform1f(timeLocation, static_cast<float>(time));
 
         // etape f. dessin de triangles dont la definition provient d'un tableau
         // le rendu s'effectue ici en prenant 3 sommets a partir du debut du tableau (0)
         glDrawArrays(GL_TRIANGLES, 0, 3);
+        // GLuint == uint32_t
+        // GLushort == uint16_t
+        //  const uint16_t indices[] = { 0, 1, 2 };
+        // glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, indices);
+
+        glDisableVertexAttribArray(POSITION);
+        glDisableVertexAttribArray(COLOR);
 
         // Il on suppose que la phase d'echange des buffers front et back
         // le « swap buffers » est effectuee juste apres
--- shaders/basic.fs.glsl
+++ shaders/basic.fs.glsl
@@ -5,8 +5,9 @@ uniform float u_Time;
 // un FS doit toujours ecrire dans 
 // gl_FragColor qui est un vec4
 
-varying vec4 v_Color;
+varying vec3 v_Color;
 
-void main(void) {
-    gl_FragColor = v_Color;
+void main()
+{
+    gl_FragColor = vec4(v_Color, 1.0);
 }
